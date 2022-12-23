a2ea7c243c7dca3888256fbf6849edbf5887b055
AzureVauban <haloringman@outlook.com>
2022-12-23 02:52:10 +0000

    Dec 22, 2022, 8:52 PM

--- main.py
+++ main.py
@@ -2,15 +2,5 @@
 final version of the Process Map (Python)
 """
 
-
-class Queue:
-    class Node:
-        after = None
-        before = None
-        data = None
-    head: Node = None
-    size: int = 0
-
-
 if __name__ == '__main__':
     print('terminating process')
