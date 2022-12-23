d3af702dbb8e25698bb348065cf4206f96828f1a
AzureVauban <haloringman@outlook.com>
2022-12-23 02:56:40 +0000

    Dec 22, 2022, 8:56 PM

--- main.py
+++ main.py
@@ -2,5 +2,15 @@
 final version of the Process Map (Python)
 """
 
+
+def fib(n: int) -> int:
+    if n <= 1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+
+
 if __name__ == '__main__':
+    for _ in range(10):
+        print(fib(_))
     print('terminating process')
