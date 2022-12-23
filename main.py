ece48f1a141657d89683aad402e0ca3cb86c1b18
AzureVauban <haloringman@outlook.com>
2022-12-23 03:14:10 +0000

    Dec 22, 2022, 9:14 PM

--- main.py
+++ main.py
@@ -9,7 +9,109 @@ def fib(n: int) -> int:
     return fib(n-1)+fib(n-2)
 
 
+class container:
+
+    class Node:
+        """Node class for pillar data structure"""
+        index: int
+        data = None
+        after = None
+        before = None
+
+        def __init__(self, before, data, after) -> None:
+            self.before = before
+            self.data = data
+            self.after = after
+            self.index = 0
+
+        def set_index(self, index):
+            """set the index of the node instance"""
+            self.index = index
+
+        def help(self):
+            """print debug data for the Node instance"""
+            print('index: '+str(self.index))
+            print('data: '+str(self.data))
+            print('after: '+str(self.after))
+            print('before: '+str(self.before))
+
+    head: Node = None
+    size: int = 0
+
+    def __init__(self) -> None:
+        self.head = None
+        self.size = 0
+
+    def __get_end(self) -> Node:
+        """get the endpoint node of the container instance"""
+        current: self.Node = self.head
+        while current.after is not None:
+            current = current.after
+        return current
+
+    def __set_index(self):
+        """set the index of all the nodes"""
+        if not self.is_empty():
+            current: self.Node = self.head
+            new_index: int = 0
+            while current.after is not None:
+                current.set_index(new_index)
+                current = current.after
+                new_index += 1
+
+    @classmethod
+    def __check_data_typing(cls, old_node: Node, new_data):
+        """make sure that the data being added is the same type"""
+        if not isinstance(old_node.data, type(new_data)):
+            raise TypeError('data is not an instance of', type(old_node.data))
+
+    def is_empty(self) -> bool:
+        """checks if there is any data in the container instance"""
+        return self.head is None
+
+    def insert_back(self, data):  # ! should be called push back/append
+        """add data to the back of the container instance"""
+        if self.is_empty():
+            # ? overwrite the head Node
+            self.head = self.Node(None, data, None)
+        else:
+            # append a new node to the end of the container instance
+            old_endpoint: self.Node = self.__get_end()
+            self.__check_data_typing(old_endpoint, data)
+            # link Node pointers of old and new endpoint
+            new_endpoint: self.Node = self.Node(old_endpoint, data, None)
+            old_endpoint.after = new_endpoint
+        # set the new indicies
+        self.__set_index()
+        # change the size of the container instance
+        self.size += 1
+
+    def remove_back(self) -> None:  # ! should be called pop back/remove back
+        """remove data from the front of the container instance"""
+        if self.is_empty():
+            raise ValueError('cannot pop any values from an empty container')
+        return_value = self.head.data
+        if self.size == 1:
+            self.head = None
+        else:
+            old_endpoint: self.Node = self.__get_end()
+            return_value = old_endpoint.data
+            new_endpoint: self.Node = None
+            if old_endpoint.before is not None:
+                # ? destroy the link the the endpoint and the node before it (if its not NULL)
+                new_endpoint = old_endpoint.before
+                new_endpoint.after = None
+                old_endpoint.before = None
+                old_endpoint = None
+                del old_endpoint
+        self.size -= 1
+        return return_value
+
+
 if __name__ == '__main__':
+    test = container()
     for _ in range(10):
-        print(fib(_))
+        test.insert_back(fib(_))
+    while not test.is_empty():
+        print(test.remove_back())
     print('terminating process')
