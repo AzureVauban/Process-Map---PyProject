c9b8ca435a2dd663a0eb47ceb236cfd43c9048c9
AzureVauban <haloringman@outlook.com>
2023-02-23 16:47:52 +0000

    Feb 23, 2023, 10:47 AM

--- myunittest.py
+++ myunittest.py
@@ -17,10 +17,11 @@ class test_setup(unittest.TestCase):
 
 
 class TestIngredientSearchAndParse(unittest.TestCase):
-
-    def testdict_parsesearch(self):
-        dict_of_head_nodes: dict = parsecsv()
-        self.assertGreaterEqual(len(dict_of_head_nodes), 5, 'errored')
+    #? instant conversion from dict to list : list(parsecsv().values())[I]
+    def testdict_parsesearch(self): #todo finish this
+        #! the first head ingredient in the csv file : list(parsecsv().values())[0]
+        prime_val = list(createtreefromcsv(list(parsecsv().values())[0]).children_ingredients.values())[2].ingredient_name
+        self.assertGreaterEqual(len(prime_val), 5, 'errored')
 
     def testsearch(self):
         #! add back later recipe : Ingredient = createtreefromcsv(parent_ingredient,False)
