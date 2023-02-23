e80c88db08f5d931542426e0978d88467ad6e2bf
AzureVauban <haloringman@outlook.com>
2023-02-23 16:54:52 +0000

    Feb 23, 2023, 10:54 AM

--- myunittest.py
+++ myunittest.py
@@ -1,6 +1,8 @@
 import unittest
 from random import randint
-from main import Ingredient,parsecsv,combine_ingredient_tree,createtreefromcsv
+from main import Ingredient, parsecsv, combine_ingredient_tree, createtreefromcsv
+
+
 def fib(n: int) -> int:
     if n <= 1:
         return 1
@@ -13,17 +15,24 @@ class test_setup(unittest.TestCase):
         fibelements: list = []
         for _ in range(10):
             fibelements.append(_+1)
-        self.assertGreaterEqual(len(fibelements),5)
+        self.assertGreaterEqual(len(fibelements), 5)
 
 
 class TestIngredientSearchAndParse(unittest.TestCase):
-    #? instant conversion from dict to list : list(parsecsv().values())[I]
-    def testdict_parsesearch(self): #todo finish this
+    # ? instant conversion from dict to list : list(parsecsv().values())[I]
+    def testdict_parsesearch(self):  # todo finish this
         #! the first head ingredient in the csv file : list(parsecsv().values())[0]
-        prime_val = list(createtreefromcsv(list(parsecsv().values())[0]).children_ingredients.values())[2].ingredient_name
-        self.assertGreaterEqual(len(prime_val), 5, 'errored')
+        prime_val = list(createtreefromcsv(list(parsecsv().values())[
+                         0]).children_ingredients.values())[2].ingredient_name
+        self.assertEqual(prime_val, 'quantum processor')
 
     def testsearch(self):
         #! add back later recipe : Ingredient = createtreefromcsv(parent_ingredient,False)
-        test_list: list = combine_ingredient_tree('')
-        self.skipTest('Not implemented')
+        test_ingredient_name: str = list(createtreefromcsv(list(parsecsv().values())[
+                                         0]).children_ingredients.values())[2].ingredient_name
+        test_ingredient_parent: Ingredient = list(createtreefromcsv(
+            list(parsecsv().values())[0]).children_ingredients.values())[2].parent_ingredient
+        test_list: list = combine_ingredient_tree(
+            test_ingredient_name, test_ingredient_parent)
+        self.assertGreaterEqual(len(test_list), 2)
+        #self.assertEqual(test_ingredient_parent.ingredient_name,'industrial battery')
